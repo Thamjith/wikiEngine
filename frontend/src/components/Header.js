43f8a27ca6817b160987007482af21a6a0c1b9a8
Thamjith <jithuthaha8@gmail.com>
2020-11-04 14:29:32 +0530

    Logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
 
@@ -10,6 +11,9 @@ const Header = () => {
     const userLogin = useSelector((state) => state.userLogin)
     const { userInfo } = userLogin
     
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
 
     return <header>
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
@@ -26,7 +30,10 @@ const Header = () => {
                         <LinkContainer to='/profile'>
                             <NavDropdown.Item>Profile</NavDropdown.Item>
                         </LinkContainer>
-                        <NavDropdown.Item>
+                        <LinkContainer to='/createArticle'>
+                            <NavDropdown.Item>Write an Article</NavDropdown.Item>
+                        </LinkContainer>
+                        <NavDropdown.Item onClick={logoutHandler}>
                             Logout
                         </NavDropdown.Item>
                         </NavDropdown>
--- frontend/src/templates/HomeScreen.js
+++ frontend/src/templates/HomeScreen.js
@@ -3,7 +3,10 @@ import React from 'react'
 const HomeScreen = () => {
     return (
         <div>
-            <h1>HomeScreen</h1>
+            <h1>Hi Netizen...Feeling Bored?</h1>
+            <h1>We provide the best in class Articles for you to read.</h1>
+            <h3>There are xx <strong>Free</strong> Articles and yy <b>Premium</b> Articles waiting for you.</h3>
+            <p>First one month Free and then only $3.99 per month</p>
         </div>
     )
 }
