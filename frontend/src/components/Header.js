6cac906905d3339b4610b1ac880220b29ceb2154
Thamjith <jithuthaha8@gmail.com>
2020-11-04 14:13:28 +0530

    sign in dropdown menu

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
+
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+    
+
     return <header>
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
         <Container>
@@ -9,8 +18,26 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                <Nav.Link href="/articles"><i className='fas fa-newspaper'></i> Articles</Nav.Link>
-                <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+                    <LinkContainer to='/articles'>
+                        <Nav.Link><i className='fas fa-newspaper'></i> Articles</Nav.Link>
+                    </LinkContainer>
+                    {userInfo ? (
+                        <NavDropdown title={userInfo.name} id='username'>
+                        <LinkContainer to='/profile'>
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
+                        </LinkContainer>
+                        <NavDropdown.Item>
+                            Logout
+                        </NavDropdown.Item>
+                        </NavDropdown>
+                    ) : (
+                        <LinkContainer to='/login'>
+                        <Nav.Link>
+                            <i className='fas fa-user'></i> Sign In
+                        </Nav.Link>
+                        </LinkContainer>
+                    )}
+                    {/* <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link> */}
                 </Nav>
             </Navbar.Collapse>
         </Container>
