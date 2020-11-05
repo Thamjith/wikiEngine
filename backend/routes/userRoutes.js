49e91b82a282758e90c492e0a29f502bf65ae596
Thamjith <jithuthaha8@gmail.com>
2020-11-05 20:18:25 +0530

    error management and protect routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,19 @@
 import express from 'express'
 const router = express.Router()
+import { protect } from '../middleware/authMiddleware.js'
 
 import {
     authUser,
     registerUser,
+    updateUserProfile
 } from '../controller/userController.js'
 
 router.post('/login', authUser)
 
 router.route('/').post(registerUser)
+
+router
+  .route('/profile')
+  .put(protect, updateUserProfile)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import articleRoutes from './routes/articlesRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -13,12 +14,16 @@ const app = express()
 
 app.use(express.json())
 
+
+app.use('/api/articles', articleRoutes)
+app.use('/api/users', userRoutes)
+
 app.get('/', (req, res) =>
   res.send('API is running...')
 )
 
-app.use('/api/articles', articleRoutes)
-app.use('/api/users', userRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
